ec5f7c900b12bff1b018e8eb591d010bf498642f
HanselV <hanselviva@gmail.com>
2023-05-27 22:25:05 -0700

    basic theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,58 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeOptions, ThemeProvider, createTheme } from "@mui/material/styles";
+
+const lightTheme: ThemeOptions = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#ff8f00",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+  typography: {
+    fontFamily: "Do Hyeon",
+  },
+  shape: {
+    borderRadius: 16,
+  },
+});
+
+const darkTheme: ThemeOptions = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#ff8f00",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+  typography: {
+    fontFamily: "Do Hyeon",
+  },
+  shape: {
+    borderRadius: 16,
+  },
+});
+
+export const ThemeWrapper: React.FC = () => {
+  return (
+    <>
+      <App />
+    </>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <ThemeWrapper />
+    </ThemeProvider>
   </React.StrictMode>
 );
