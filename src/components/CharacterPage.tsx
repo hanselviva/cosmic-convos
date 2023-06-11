a6aef53627b9ae505ec61af9ccc8666c0516e5b0
HanselV <hanselviva@gmail.com>
2023-06-10 20:09:03 -0700

    chat page now scrolls to chat div onload

--- src/components/CharacterPage.tsx
+++ src/components/CharacterPage.tsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { universeData } from "./_universeData";
 import { Container, Typography } from "@mui/material";
 import Chatbox from "./Chatbox";
 
 const CharacterPage: React.FC = () => {
+  const divRef = useRef<HTMLDivElement>(null);
   const { universeId, characterId } = useParams();
   const universe = universeData.filter((data) => data.id == universeId)[0].data;
   const { name, id, avatar } = universe.characters.filter(
     (char) => char.id == characterId
   )[0];
 
+  useEffect(() => {
+    if (divRef.current) {
+      divRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, []);
+
   return (
-    <div className="content-container">
+    <div className="content-container" ref={divRef}>
       <Typography variant="h4" component="h1" sx={{ my: "2rem" }}>
         <img height="75px" width="75px" src={avatar} /> Chat with {name}
       </Typography>
